ab137f8d40afd70e73d1e4385d7bbf924583abe2
Viraj Parab <virajpa@thoughtworks.com>
2016-11-27 22:13:57 +0530

    cl should handle a file and also work as repl

--- cl.js
+++ cl.js
@@ -1,25 +1,37 @@
-
 var Parser = require('jison').Parser;
-var grammer = require('fs').readFileSync('./source/jison/clGrammer.jison', 'utf-8');
+var fs = require('fs');
+var grammer = fs.readFileSync('./source/jison/clGrammer.jison', 'utf-8');
 var parser = new Parser(grammer);
 var readline = require('readline');
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
 
-rl.on('line', function(line){
-	var tree;
-	tree = parser.parse(line);
-	try{
-		if(line == "")
-			return ;	
-		var result = tree.evaluate();
-		console.log(result.getValue());
-	}catch(e){
-    	console.log("syntax error:",e);
-    	tree.removeLastBranch();
-	}
-});
+var readFromReadLine = function(){
+	var rl = readline.createInterface({
+			  input: process.stdin,
+			  output: process.stdout,
+			  terminal: false
+			});
 
+	rl.on('line', function(line){
+		var tree;
+		tree = parser.parse(line);
+		try{
+			if(line == "")
+				return ;	
+			var result = tree.evaluate();
+			console.log(result.getValue());
+		}catch(e){
+	    	console.log("syntax error:",e);
+	    	tree.removeLastBranch();
+		}
+	});
+}
+
+var givenFilePath = process.argv[2];
+if(givenFilePath){
+	var data = fs.readFileSync(givenFilePath, 'utf-8');
+	var tree = parser.parse(data);
+	var result = tree.evaluate();
+	console.log(result.getValue());
+}else{
+	readFromReadLine();
+}
